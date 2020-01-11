630d1fb1e5b9ca66713cf12ef89d9bc8ccb9fe49
Marcin Szymanski <ms32035@gmail.com>
2020-01-11 16:52:27 +0100

    fix entrypoint

--- enviroplus2mqtt/run.sh
+++ enviroplus2mqtt/run.sh
@@ -2,9 +2,11 @@
 
 CONFIG_PATH=/data/options.json
 
-DATA_PATH=$(jq --raw-output ".data_path" $CONFIG_PATH)
-if [[ $(jq --raw-output ".data_path" $CONFIG_PATH) == "true" ]] then
-  PMS5003="--use-5003"
+USE_PMS5003=$(jq --raw-output ".use_pms5003" $CONFIG_PATH)
+
+if [[ "${USE_PMS5003}" == "true" ]]
+then
+  PMS5003="--use-pms5003"
 else
   PMS5003=""
 fi
@@ -14,6 +16,6 @@ python3 /enviroplus2mqtt/enviroplus-mqtt/src/main.py \
 --port $(jq --raw-output ".port" $CONFIG_PATH) \
 --username $(jq --raw-output ".username" $CONFIG_PATH) \
 --password $(jq --raw-output ".password" $CONFIG_PATH) \
---client-id $(jq --raw-output ".client-id" $CONFIG_PATH) \
+--client-id $(jq --raw-output ".client_id" $CONFIG_PATH) \
 --interval $(jq --raw-output ".interval" $CONFIG_PATH) \
 --delay $(jq --raw-output ".delay" $CONFIG_PATH) $PMS5003
